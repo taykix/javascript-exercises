1099ab5844b109a9f703bf1cfde6e9a22c942b9a
t.karakaya <t.karakaya@tu-bs.de>
2025-09-21 15:49:24 +0200

    js ex 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(input, num) {
+    let strAcc = input;
+    if (num === 0) {
+        return '';
+    } else if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 1; i < num; i++) {
+        strAcc += input;
+    }
 
+    return strAcc;
 };
 
 // Do not edit below this line
